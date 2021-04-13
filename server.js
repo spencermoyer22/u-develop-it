63ee523aa92e89e6b3961aee50d8159e7d453d4c
Big Head <spencer.moyer16@yahoo.com>
2021-04-13 13:51:56 -0700

    created api routes for querying candidates and parties tables together

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ const db = mysql.createConnection(
 );
 
 app.get('/api/candidates', (req, res) => {
-    const sql = `SELECT * FROM candidates`;
+    const sql = `SELECT candidates.*, parties.name AS party_name
+                FROM candidates
+                LEFT JOIN parties ON candidates.party_id = parties.id`;
     db.query(sql, (err, rows) => {
         if (err) {
             res.status(500).json({ error: err.message });
@@ -32,7 +34,10 @@ app.get('/api/candidates', (req, res) => {
 
 // get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const sql = `SELECT candidates.*, parties.name AS party_name
+                FROM candidates
+                LEFT JOIN parties ON candidates.party_id = parties.id
+                WHERE candidates.id = ?`;
     const params = [req.params.id];
 
     db.query(sql, params, (err, row) => {
